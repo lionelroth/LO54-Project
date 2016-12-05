caa6eede3a651cf8798ef8c58beb657dfca8ef9f
lionelroth <lionel.roth@utbm.fr>
2016-12-05 15:33:01 +0100

    [test] wtf

---
+++ formation-app/formation-app-core/src/main/java/com/utbm/lo54/formation_app/core/App.java
@@ -0,0 +1,13 @@
+package com.utbm.lo54.formation_app.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
